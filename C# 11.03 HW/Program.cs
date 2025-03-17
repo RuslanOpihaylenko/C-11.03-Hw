059115a7c95a48127cba80301227b0792edb86af
RuslanOpihaylenko <rysya08shkolnik@gmail.com>
2025-03-17 14:35:14 +0200

    first commit

---
+++ C# 11.03 HW/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace C__11._03_HW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> arif = x =>
+            {
+                if (x % 1 == 0 && x % x == 0)
+                { 
+                    if (x % x-1 != 0 || x % x+1 != 0)
+                    {
+                        Console.WriteLine($"Number {x} is proste!");
+                    }
+                }
+                if (x + 2 ==(x+x+1))
+                {
+                    Console.WriteLine($"Number {x} is Fibonachi!");
+                }
+                if (x % 2 == 0)
+                {
+                    Console.WriteLine($"Number {x} is parne!");
+                }
+                else
+                {
+                    Console.WriteLine($"Number {x} is ne parne!");
+                }
+            };
+            arif.Invoke(4);
+        }
+    }
+}
